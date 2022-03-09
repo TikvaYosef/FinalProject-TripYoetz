b8960322f8f93f35b52653c3ba5c9c10818e8a01
Marcos Bazbih <marcosbaza100@gmail.com>
2022-03-09 17:44:26 +0200

    activities CRUD done

--- server/controllers/activity-controller.js
+++ server/controllers/activity-controller.js
@@ -10,5 +10,45 @@ module.exports = {
         catch (err) {
             res.status(500).json({ success: false, message: err.message });
         }
+    },
+    AddActivities: async (req, res) => {
+        try {
+            const { name, city, location, phone, activitiesHours, images, comments, greenPass, rating, link, q_a } = req.body;
+            const activity = new activities({ name, city, location, phone, activitiesHours, images, comments, greenPass, rating, link, q_a });
+            if (!activity) return res.status(400).json({ success: false, message: "activity not valid" })
+
+            await activities.create(activity)
+                .then(() => res.status(201).json({ success: true, message: "activity successfully added" }))
+                .catch((err) => res.status(400).json({ success: false, message: err.message }))
+        }
+        catch (err) {
+            res.status(500).json({ success: false, message: err.message });
+        }
+    },
+    UpdateActivities: async (req, res) => {
+        try {
+            if (await activities.exists({ _id: req.params.id })) {
+                return await activities.findByIdAndUpdate(req.params.id, req.body)
+                    .then(() => res.status(200).json({ success: true, message: "activity updated successfully" }))
+                    .catch((err) => res.status(400).json({ success: false, message: err.message }))
+            }
+            res.status(404).json({ success: false, message: "no activity found" });
+        }
+        catch (err) {
+            res.status(500).json({ success: false, message: err.message });
+        }
+    },
+    DeleteActivities: async (req, res) => {
+        try {
+            if (await activities.exists({ _id: req.params.id })) {
+                return await activities.findByIdAndRemove(req.params.id)
+                    .then(() => res.status(200).json({ success: true, message: "activity deleted successfully" }))
+                    .catch(err => res.status(400).json({ success: false, message: err.message }))
+            }
+            res.status(404).json({ success: false, message: "no activity found" });
+        }
+        catch (err) {
+            res.status(500).json({ success: false, message: err.message });
+        };
     }
 }
