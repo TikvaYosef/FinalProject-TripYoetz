f121d136141308e27efbf5b1f3262ffb7925f00b
Marcos Bazbih <marcosbaza100@gmail.com>
2022-03-09 18:41:22 +0200

    user routes and server.js

--- server/server.js
+++ server/server.js
@@ -1,19 +1,25 @@
 require("dotenv").config()
-const express = require("express")
 require("./db")
+
+const express = require("express")
 const cors = require("cors");
-
-const app = express();
-app.use(express.json());
-app.use(cors());
 const PORT = process.env.PORT || 9090
-app.listen(PORT, () => console.log("server up"))
-
-
+const passport = require("passport");
+const app = express();
 const citiesRoutes = require("./routes/city-routes");
 const hotelsRoutes = require("./routes/hotel-routes");
 const restaurantsRoutes = require("./routes/restaurant-routes")
 const activitiesRoutes = require("./routes/activity-routes")
+
+require("./config/passport")(passport);
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+
+app.listen(PORT, () => console.log("server up"))
+
+app.use(passport.initialize());
 
 app.use("/api/cities", citiesRoutes);
 app.use("/api/hotels", hotelsRoutes);
