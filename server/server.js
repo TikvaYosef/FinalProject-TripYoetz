1e80222c3b2b5a598915740b0889cced2f787e2d
TikvaYosef <tikvayosef13@gmail.com>
2022-03-09 18:47:42 +0200

    commit

--- server/server.js
+++ server/server.js
@@ -4,13 +4,8 @@ require("./db")
 const express = require("express")
 const cors = require("cors");
 const PORT = process.env.PORT || 9090
-<<<<<<< HEAD
-app.listen(PORT, () => console.log("server up"))
-
-=======
 const passport = require("passport");
 const app = express();
->>>>>>> d6a40a6f8447d972f9ae8a7e209b07caeb5eaa80
 const citiesRoutes = require("./routes/city-routes");
 const hotelsRoutes = require("./routes/hotel-routes");
 const restaurantsRoutes = require("./routes/restaurant-routes")
