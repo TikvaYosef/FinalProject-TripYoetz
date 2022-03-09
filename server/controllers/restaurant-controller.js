41e42856f520f32f37640837fab9b30d2331cac3
TikvaYosef <tikvayosef13@gmail.com>
2022-03-09 18:17:25 +0200

    restaurants and cities

--- server/controllers/restaurant-controller.js
+++ server/controllers/restaurant-controller.js
@@ -10,5 +10,47 @@ module.exports = {
         catch (err) {
             res.status(500).json({ success: false, message: err.message });
         }
+    },
+    AddRestaurant: async (req, res) => {
+        try {
+            const { name, city, description, images, location, phone, comments, greenPass, rating, q_a, link, activitiesHours } = req.body;
+            const restaurant = new restaurants({ name, city, description, images, location, phone, comments, greenPass, rating, q_a, link, activitiesHours });
+            if (!restaurant) return res.status(400).json({ success: false, message: "restaurant not valid" })
+
+            await restaurants.create(restaurant)
+                .then(() => res.status(201).json({ success: true, message: "restaurant successfully added" }))
+                .catch((err) => res.status(400).json({ success: false, message: err.message }))
+        }
+        catch (err) {
+            res.status(500).json({ success: false, message: err.message });
+        }
+    },
+    
+    UpdateRestaurant: async (req, res) => {
+        try {
+            if (await restaurants.exists({ _id: req.params.id })) {
+                return await restaurants.findByIdAndUpdate(req.params.id, req.body)
+                    .then(() => res.status(200).json({ success: true, message: "restaurant updated successfully" }))
+                    .catch((err) => res.status(400).json({ success: false, message: err.message }))
+            }
+            res.status(404).json({ success: false, message: "no restaurant found" });
+        }
+        catch (err) {
+            res.status(500).json({ success: false, message: err.message });
+        }
+    },
+
+    DeleteRestaurant: async (req, res) => {
+        try {
+            if (await restaurants.exists({ _id: req.params.id })) {
+                return await restaurants.findByIdAndRemove(req.params.id)
+                    .then(() => res.status(200).json({ success: true, message: "restaurant deleted successfully" }))
+                    .catch(err => res.status(400).json({ success: false, message: err.message }))
+            }
+            res.status(404).json({ success: false, message: "no restaurant found" });
+        }
+        catch (err) {
+            res.status(500).json({ success: false, message: err.message });
+        };
     }
 }
--- server/routes/restaurant-routes.js
+++ server/routes/restaurant-routes.js
@@ -1,7 +1,10 @@
 const restaurantsRoutes = require("express").Router();
 
-const { GetRestaurants } = require("../controllers/restaurant-controller");
+const { GetRestaurants, AddRestaurant, UpdateRestaurant, DeleteRestaurant } = require("../controllers/restaurant-controller");
 
 restaurantsRoutes.get("/", GetRestaurants);
+restaurantsRoutes.post("/", AddRestaurant);
+restaurantsRoutes.put("/:id", UpdateRestaurant)
+restaurantsRoutes.delete("/:id", DeleteRestaurant)
 
 module.exports = restaurantsRoutes;
