e5784741129b7b7ba04a9b663a8b4512f9f5b211
TikvaYosef <tikvayosef13@gmail.com>
2022-03-10 13:33:07 +0200

    restaurants-rest

--- client/src/services/restaurant-services.js
+++ client/src/services/restaurant-services.js
@@ -14,51 +14,51 @@ export const GetRestaurants = async () => {
         console.log(err);
     }
 }
-// export const AddRestaurant = async (restaurant) => {
-//     const options = {
-//         method: "POST",
-//         body: JSON.stringify({ ...restaurant }),
-//         headers: { 'Content-Type': 'application/json' }
-//     }
-//     try {
-//         return await fetch(`${BASE_URL}`, options)
-//             .then((res) => res.json())
-//             .then((res) => console.log(res))
-//     }
-//     catch (err) {
-//         console.log(err);
-//     }
-// }
+export const AddRestaurant = async (restaurant) => {
+    const options = {
+        method: "POST",
+        body: JSON.stringify({ ...restaurant }),
+        headers: { 'Content-Type': 'application/json' }
+    }
+    try {
+        return await fetch(`${BASE_URL}/api/restaurants`, options)
+            .then((res) => res.json())
+            .then((res) => console.log(res))
+    }
+    catch (err) {
+        console.log(err);
+    }
+}
 
 
-// export const UpdateRestaurant = async (restaurant) => {
-//     const options = {
-//         method: "PUT",
-//         body: JSON.stringify({ ...restaurant }),
-//         headers: { 'Content-Type': 'application/json' }
-//     }
-//     try {
-//         return await fetch(`${BASE_URL}`, options)
-//             .then((res) => res.json())
-//             .then((res) => console.log(res))
-//     }
-//     catch (err) {
-//         console.log(err);
-//     }
-// }
+export const UpdateRestaurant = async (id,restaurant) => {
+    const options = {
+        method: "PUT",
+        body: JSON.stringify({ ...restaurant }),
+        headers: { 'Content-Type': 'application/json' }
+    }
+    try {
+        return await fetch(`${BASE_URL}/api/restaurants/${id}`, options)
+            .then((res) => res.json())
+            .then((res) => console.log(res))
+    }
+    catch (err) {
+        console.log(err);
+    }
+}
 
-// export const DeleteRestaurant = async (id) => {
-//     const options = {
-//         method: "DELETE",
-//     };
-//     try {
-//         return await fetch(`${BASIC_API}/${id}`, options)
-//             .then((res) => res.json())
-//             .then((res) => console.log(res))
-//     }
-//     catch (err) {
-//         console.log(err);
-//     }
-// }
+export const DeleteRestaurant = async (id) => {
+    const options = {
+        method: "DELETE",
+    };
+    try {
+        return await fetch(`${BASE_URL}/api/restaurants/${id}`, options)
+            .then((res) => res.json())
+            .then((res) => console.log(res))
+    }
+    catch (err) {
+        console.log(err);
+    }
+}
 
 
