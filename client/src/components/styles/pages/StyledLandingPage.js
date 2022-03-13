fe0ab6ab01bd85b0c0e269251d35c4fc674e86fc
TikvaYosef <tikvayosef13@gmail.com>
2022-03-13 20:18:08 +0200

    landingpAGE

---
+++ client/src/components/styles/pages/StyledLandingPage.js
@@ -0,0 +1,55 @@
+import styled from "styled-components";
+
+
+export const StyledLandingPage = styled.section`
+text-align: center;
+font-family: 'Lobster', cursive; 
+
+
+
+
+& .title{
+  font-weight: 200px;
+text-align: center;
+
+}
+
+& .dataArticale{
+    
+    display: flex;
+    flex-direction: row;
+    justify-content: space-around;
+    margin-top: 50px;
+}
+
+& .data{
+    border: 2px solid black;
+    border-radius: 10px;
+}
+& .aboutus{
+    border: 2px solid black;
+    border-radius: 10px;
+    margin-top: 50px;
+
+  
+}
+& .phone{
+  display:flex ;
+  flex-direction: row;
+  justify-content: center;
+
+}
+
+& .imgphone{
+width: 15px;
+height: 20px;
+}
+& .Activitytime{
+    width: 50px;
+    height: 30px; 
+}
+
+    
+
+`
+
