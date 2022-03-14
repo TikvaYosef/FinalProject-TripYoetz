e7400483f2a1ecf0506b8c19a8855ef099ecc4a9
TikvaYosef <tikvayosef13@gmail.com>
2022-03-14 13:14:21 +0200

    categories_func

--- client/src/components/pages/Hotels.jsx
+++ client/src/components/pages/Hotels.jsx
@@ -1,33 +1,30 @@
-import { useContext, useEffect, useState } from 'react'
+import { useContext, useEffect } from 'react'
 import { MainContext } from '../../contexts/main-context';
 import { GetHotels } from "../../services/hotel-services.js"
 import { GetData } from "../../state-management/actions/categories-actions"
 import Navbar from '../layout/Navbar';
 
 const Hotels = () => {
-    const { hotels, hotelsDispatch } = useContext(MainContext);
-    const [search, setSearch] = useState("");
+    const { hotels, hotelsDispatch,city } = useContext(MainContext);
 
-    const HandleOnInput = (event) => {
-        setSearch(event.target.value);
-    }
 
-    const SendSearchFrom = (event) => {
-        event.preventDefault();
+    useEffect(() => {
         GetHotels()
-            .then((res) => { hotelsDispatch(GetData(res.data.filter((hotel) => hotel.city === search))) })
-    }
+            .then(res => {
+                hotelsDispatch(
+                    GetData(res.data.filter(item => item.city === city.name))
+                )
+            })
+    }, []);
+
+
+
     return (
         <div>
             <Navbar />
-            {hotels.length >= 1
-                ?
-                <button onClick={() => { console.log(hotels) }}>Click</button>
-                :
-                <form className="search-form" onSubmit={SendSearchFrom}>
-                    <input className="search-input" type="text" onInput={HandleOnInput} placeholder="where do you want to travel?" />
-                </form>
-            }
+
+            <button onClick={() => { console.log(hotels) }}>Click</button>
+
         </div>
     )
 }
