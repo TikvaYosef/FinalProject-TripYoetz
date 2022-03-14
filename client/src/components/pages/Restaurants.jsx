6909619d0fb5e33025355172e2fbe0829fb86a3a
TikvaYosef <tikvayosef13@gmail.com>
2022-03-14 12:29:51 +0200

    restaurantfunc

--- client/src/components/pages/Restaurants.jsx
+++ client/src/components/pages/Restaurants.jsx
@@ -1,6 +1,6 @@
 import { useContext, useEffect } from 'react'
 import { MainContext } from '../../contexts/main-context';
-import { GetRestaurants, GetRestaurantByName } from "../../services/restaurant-services"
+import { GetRestaurants } from "../../services/restaurant-services"
 import { GetData } from "../../state-management/actions/categories-actions"
 import Navbar from '../layout/Navbar';
 
@@ -8,18 +8,17 @@ const Restaurants = () => {
     const { restaurants, restaurantsDispatch, city } = useContext(MainContext);
 
     useEffect(() => {
-        GetRestaurantByName()
+        GetRestaurants()
             .then(res => {
                 restaurantsDispatch(
-                    GetData(res.data.filter(item => item.city === city))
+                    GetData(res.data.filter(item => item.city === city.name))
                 )
             })
-        // .then((res) => {restaurantsDispatch(GetData(res.data))})
     }, []);
 
     return (
         <div>
-            <Navbar/>
+            <Navbar />
             Restaurants
             <button onClick={() => { console.log(restaurants) }}>Click</button>
         </div>
