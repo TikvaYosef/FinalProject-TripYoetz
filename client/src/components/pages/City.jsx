45ba5aead5b1295efed1d23bcd3e6f2dbc35a9a2
Marcos Bazbih <marcosbaza100@gmail.com>
2022-03-14 13:26:38 +0200

    city page fix

--- client/src/components/pages/City.jsx
+++ client/src/components/pages/City.jsx
@@ -11,9 +11,15 @@ const City = () => {
             <button onClick={() => { console.log(city) }}>Click</button>
             <h1>{city.name}</h1>
             <h1>{city.description}</h1>
-            <img src={city.images[0]} alt="img 1" />
-            <img src={city.images[1]} alt="img 2" />
-            <img src={city.images[2]} alt="img 3" />
+            {
+                city.images
+                    ?
+                    city.images.map((img) =>
+                        <img src={img} alt='img' />
+                    )
+                    :
+                    null
+            }
         </div>
     )
 }
