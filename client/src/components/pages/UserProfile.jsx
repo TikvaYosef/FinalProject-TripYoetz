081ccc5d94b7fdaf85a346ba8451726e7160cd68
avi-admaso <aviadmaso@gmail.com>
2022-03-16 16:21:53 +0200

    design user profile page

--- client/src/components/pages/UserProfile.jsx
+++ client/src/components/pages/UserProfile.jsx
@@ -3,32 +3,45 @@ import { MainContext } from "../../contexts/data-context";
 import { StyledUserProfile } from "../styles/pages/StyledUserProfile";
 
 const UserProfile = () => {
-  const { user,restaurants } = useContext(MainContext);
-
+  const { user, restaurants } = useContext(MainContext);
+  function getTheComments() {
+    let counter = 0;
+    for (const restaurant of restaurants) {
+      for (const commentItem of restaurant.comments) {
+        if (commentItem.user_id === user._id) {
+          counter++;
+        }
+      }
+    }
+    return counter;
+  }
   return (
     <StyledUserProfile>
-        {/* <img src="https://superhqwallpapers.com/wp-content/uploads/2021/09/Amazing-Nature-View.jpg" alt="" /> */}
-      <div>
-        {/* <h1>User</h1> */}
-        <img src={user.image} alt="img" />
-        <h1>{user.name}</h1>
-        <h1>{user.lastName}</h1>
-        <button>Your Activity</button>
-        {/* <h1>{user.email}</h1>
-        <h1>{user.birthDate}</h1> */}
-        <h1><button onClick={() => {
-          let counter =0;
-          for (const restaurant of restaurants) {
-            for (const commentItem of restaurant.comments) {
-              if (commentItem.user_id === user._id) {
-                counter++;
-              }
-            }
-          }
-        console.log(counter);
-        // {counter}
-        }}>aaaa</button></h1>
-        
+      {/* <img src="https://superhqwallpapers.com/wp-content/uploads/2021/09/Amazing-Nature-View.jpg" alt="" /> */}
+      <div className="mainDiv">
+        <img className="profileImg" src={user.image} alt="img" />
+        <div className="userActs">          
+              <h1>
+                {`Comments  ${getTheComments()}`}
+              </h1>
+              <button>Favorites <i class="far fa-heart"></i></button>
+          <h1>likes 0</h1>
+        </div>
+        <button className="editIcon">
+          <i class="fas fa-edit"> edit profile</i>
+        </button>
+      </div>
+      <div className="detailsAndTable">
+      <div className="userDetails">
+              <h1>YOUR INFO</h1>
+        <ul>
+              <h3><li>{user.name}</li></h3>
+              <h3><li>{user.lastName}</li></h3>
+              <h3><li>{user.email}</li></h3>
+              <h3><li>{user.birthDate.slice(0, 10)}</li></h3>
+
+        </ul>
+    
       </div>
       <table>
   <tr>
@@ -40,6 +53,7 @@ const UserProfile = () => {
   <tr>
   </tr>
 </table>
+        </div>
     </StyledUserProfile>
   );
 };
