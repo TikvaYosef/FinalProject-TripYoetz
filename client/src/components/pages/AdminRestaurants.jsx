a97c26b52bba851177abe7de2d259f45a7e4ffe0
TikvaYosef <tikvayosef13@gmail.com>
2022-03-17 12:49:27 +0200

    admin restaurants-delete and update

--- client/src/components/pages/AdminRestaurants.jsx
+++ client/src/components/pages/AdminRestaurants.jsx
@@ -1,11 +1,12 @@
-import React, { useContext, useEffect } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import { MainContext } from '../../contexts/data-context';
-import { GetRestaurants } from '../../services/restaurant-services';
+import { GetRestaurants, DeleteRestaurant, UpdateRestaurant } from '../../services/restaurant-services';
 import { GetData } from '../../state-management/actions/categories-actions';
 import { StyledAdminRestaurants } from '../styles/pages/StyledAdminRestaurants';
 
 const AdminRestaurants = () => {
     const { restaurants, restaurantsDispatch } = useContext(MainContext)
+    const [updateItem, setupdateItem] = useState({})
 
     useEffect(() => {
         GetRestaurants()
@@ -16,10 +17,84 @@ const AdminRestaurants = () => {
             })
     }, []);
 
+    const Delete = (id) => {
+        DeleteRestaurant(id)
+            .then((res) => { console.log(res) })
+
+        GetRestaurants()
+            .then(res => {
+                restaurantsDispatch(
+                    GetData(res.data)
+                )
+            })
+    }
+    const handleFormOnInput = (event) => {
+        updateItem[event.target.name] = event.target.value;
+
+    }
+
+
+    const Update = (id, item) => {
+        setupdateItem({ ...item })
+        UpdateRestaurant(id, updateItem)
+            .then((res) => { console.log(res); })
+
+        GetRestaurants()
+            .then(res => {
+                restaurantsDispatch(
+                    GetData(res.data)
+                )
+            })
+    }
+
+
+
     return (
         <StyledAdminRestaurants>
             <div>
+                <label htmlFor="text">Name</label>
+                <input></input>
+
+                <label htmlFor="text">City</label>
+                <input></input>
+
+                <label htmlFor="text">Description</label>
+                <input></input>
+
+                <label htmlFor="text">images</label>
+                <input></input>
+
+                <label htmlFor="text">location</label>
+                <input></input>
+
+                <label htmlFor="text">phone</label>
+                <input></input>
+
+                <label htmlFor="text">comments</label>
+                <input></input>
+
+                <label htmlFor="text">greenPass</label>
+                <input></input>
+
+                <label htmlFor="text">rating</label>
+                <input></input>
+
+                <label htmlFor="text">q_a</label>
+                <input></input>
+
+                <label htmlFor="text">link</label>
+                <input></input>
+
+
+                <label htmlFor="text">activitiesHours</label>
+                <input></input>
+                <br />
+
+                <button>Update</button>
+
                 <table>
+
+
                     <tr className='tr'>
                         <td className='td'>Name</td>
                         <td className='td'>City</td>
@@ -38,21 +113,32 @@ const AdminRestaurants = () => {
                     {
                         restaurants.map((item) =>
                             <tr className='tr'>
-                                <td className='td'>{item.name}</td>
-                                <td className='td'>{item.city}</td>
-                                <td  className ='description' >{item.description}</td>
-                                <td className='td'><img src={item.images} /></td>
-                                <td className='td'>{item.location}</td>
-                                <td className='td'>{item.phone}</td>
-                                <td className='td'>{item.comments.length}</td>
-                                <td className='td'>{item.greenPass}</td>
-                                <td className='td'>{item.rating.length}</td>
-                                <td className='td'>{item.q_a.length}</td>
-                                <td className='td'>{item.link}</td>
-                                <td className='td'F>{item.activitiesHours}</td>
+                                <td className='td'>{item.name} <input name='name' onChange={handleFormOnInput} /></td>
+                                <td className='td'>{item.city} <input name='city' onChange={handleFormOnInput} /></td>
+                                <td className='description'>{item.description} <input name='description' onChange={handleFormOnInput} /></td>
+                                <td className='td'><img src={item.images} /> <input name='images' onChange={handleFormOnInput} /></td>
+                                <td className='td'>{item.location} <input name='location' onChange={handleFormOnInput} /></td>
+                                <td className='td'>{item.phone}<input name='phone' onChange={handleFormOnInput} /></td>
+                                <td className='td'>{item.comments.length} <input name='comments' onChange={handleFormOnInput} /></td>
+                                <td className='td'>{item.greenPass} <input name='greenPass' onChange={handleFormOnInput} /></td>
+                                <td className='td'>{item.rating.length} <input name='rating' onChange={handleFormOnInput} /></td>
+                                <td className='td'>{item.q_a.length}<input name='q_a' onChange={handleFormOnInput} /></td>
+                                <td className='td'>{item.link}<input name='link' onChange={handleFormOnInput} /></td>
+                                <td className='td'>{item.activitiesHours}<input name='activitiesHours' onChange={handleFormOnInput} /></td>
+                                <tr>
+                                    <td><button onClick={() => Delete(item._id)}>Delete</button></td>
+                                </tr>
+                                <tr>
+                                    <td><button onClick={() => { Update(item._id, item) }}>Update</button></td>
+                                </tr>
+                                <tr></tr>
                             </tr>
+
+
+
                         )
                     }
+
                 </table>
             </div>
         </StyledAdminRestaurants>
