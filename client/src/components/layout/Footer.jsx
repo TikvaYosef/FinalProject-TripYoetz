b5a8c139a78273c5c6503cfd16337927040ace82
Marcos Bazbih <marcosbaza100@gmail.com>
2022-03-19 21:19:22 +0200

    footer done

--- client/src/components/layout/Footer.jsx
+++ client/src/components/layout/Footer.jsx
@@ -1,18 +1,43 @@
-import { useContext } from "react";
+import { useContext, useRef, useState } from "react";
 import { ThemeContext } from "../../contexts/theme-context";
 import { StyledFooter } from "../styles/layout/StyledFooter.js";
 import ContactFooter from "../parts/ContactFooter.jsx";
+import TravelExploreIcon from '@mui/icons-material/TravelExplore';
+import { Link } from "react-router-dom";
+import ArrowCircleUpIcon from '@mui/icons-material/ArrowCircleUp';
 
 const Footer = () => {
   const { mode } = useContext(ThemeContext);
+  const [toggle, setToggle] = useState(true);
+  const arrowRef = useRef();
+
+  const toggleFooter = () => {
+    setToggle(!toggle);
+    arrowRef.current.classList.toggle("flip-arrow");
+  }
 
   return (
-    <StyledFooter mode={mode}>
-      <ContactFooter name="Marcos" email="marcosbazbih@gmail.com" linkedin="https://www.linkedin.com/in/marcos-bazbih/" github="https://github.com/Marcos-Bazbih" img="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png" />
-      <ContactFooter name="Tikva" email="tikvayosef13@gmail.com" linkedin="https://www.linkedin.com/in/tikva-yosef-406bba223/" github="https://github.com/TikvaYosef" img="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png" />
-      <ContactFooter name="Avi" email="aviadmaso@gmail.com" linkedin="https://www.linkedin.com/in/avi-admaso-a47b13218/" github="https://github.com/avi-admaso" img="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png" />
+    <StyledFooter toggle={toggle} mode={mode}>
+      <ContactFooter name="Marcos Bazbih" info="24 years old, Ashdod" email="marcosbazbih@gmail.com" linkedin="https://www.linkedin.com/in/marcos-bazbih/" github="https://github.com/Marcos-Bazbih" img="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png" />
+      <ContactFooter name="Tikva Yosef" info="26 years old, Natanya" email="tikvayosef13@gmail.com" linkedin="https://www.linkedin.com/in/tikva-yosef-406bba223/" github="https://github.com/TikvaYosef" img="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png" />
+      <ContactFooter name="Avi Admaso" info="26 years old, Ashdod" email="aviadmaso@gmail.com" linkedin="https://www.linkedin.com/in/avi-admaso-a47b13218/" github="https://github.com/avi-admaso" img="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png" />
+      <button onClick={toggleFooter} className="open-footer-btn">
+        <ArrowCircleUpIcon ref={arrowRef} className="open-footer-icon" />
+        Who are we?
+      </button>
+      <div className="logo">
+        <Link to={"/"} className="TripYoetz-logo">
+          <TravelExploreIcon className="logo-icon" />
+          TripYoetz
+        </Link>
+        <Link className="about-us-link" to={"/about"}>Learn more</Link>
+        <h2 className="copyright">copyright © | 2022 TripYoetz | all right reserved.</h2>
+      </div>
     </StyledFooter>
   );
 };
 
 export default Footer;
+
+
+
--- client/src/components/parts/ContactFooter.jsx
+++ client/src/components/parts/ContactFooter.jsx
@@ -1,14 +1,18 @@
+import { useContext } from "react";
+import { ThemeContext } from "../../contexts/theme-context";
 import { StyledContactFooter } from "../styles/parts/StyledContactFooter";
 
-const ContactFooter = ({ name, email, linkedin, github, img }) => {
+const ContactFooter = ({ name, info, email, linkedin, github, img }) => {
+    const { mode } = useContext(ThemeContext);
     return (
-        <StyledContactFooter>
-            <h1 className="contact-name">{name}</h1>
-            <img className="image-popup" src={img} alt={`${name} img`} />
+        <StyledContactFooter className={name} mode={mode}>
+            <h1 className="developer-name">{name}</h1>
+            <h2 className="developer-info">{info}</h2>
+            <img className="developer-image" src={img} alt={`${name} img`} />
             <div className="contact-icons-box">
-                <a className="icon-link" href={email}><i className="fa fa-envelope"></i></a>
-                <a className="icon-link" href={linkedin}><i className="fa fa-linkedin"></i></a>
-                <a className="icon-link" href={github}><i className="fa fa-github"></i></a>
+                <a href={`mailto:${email}`}><i className="fa fa-envelope contact-icon"></i></a>
+                <a target="_blank" rel="noreferrer" href={linkedin}><i className="fa fa-linkedin contact-icon"></i></a>
+                <a target="_blank" rel="noreferrer" href={github}><i className="fa fa-github contact-icon"></i></a>
             </div>
         </StyledContactFooter>
     )
