942f378cbe8ea47854b9d366b4512577c4c9732b
TikvaYosef <tikvayosef13@gmail.com>
2022-03-21 14:18:21 +0200

    admin navbar stayle

--- client/src/components/pages/AdminRoutes.jsx
+++ client/src/components/pages/AdminRoutes.jsx
@@ -1,14 +1,17 @@
 import { Link, Outlet } from "react-router-dom";
+import { StyledAdminNavbar } from '../styles/layout/StyledAdminNavbar'
 
 const AdminRoutes = () => {
 
   return (
     <>
-      <ul>
-        <li><Link to="AdminRestaurants">Restaurants</Link></li>
-        <li><Link to="AdminHotels">Hotels</Link></li>
-        <li><Link to="AdminActivities">Activities</Link></li>
-      </ul>
+      <StyledAdminNavbar>
+        <ul className="navbar-list" >
+          <li><Link className="navbar-link" to="AdminRestaurants">Restaurants</Link></li>
+          <li><Link className="navbar-link" to="AdminHotels">Hotels</Link></li>
+          <li><Link className="navbar-link" to="AdminActivities">Activities</Link></li>
+        </ul>
+      </StyledAdminNavbar>
       <Outlet />
     </>
   );
