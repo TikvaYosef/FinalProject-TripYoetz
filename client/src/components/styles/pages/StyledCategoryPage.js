4d2faad38eae2da7d1f5a0b9321525ad1c20d86d
avi-admaso <aviadmaso@gmail.com>
2022-03-24 19:47:52 +0200

    responsive category page part1

--- client/src/components/styles/pages/StyledCategoryPage.js
+++ client/src/components/styles/pages/StyledCategoryPage.js
@@ -57,4 +57,35 @@ export const StyledCategoryPage = styled.section`
         background: ${({mode}) => mode.color};
         color: ${({mode}) => mode.background};
     }
-`;
+    @media  only screen  and (min-width:320px) and (max-width:375px){
+        .sort-fixed{
+        width: 15vw;
+        height: 10vh;
+        display: flex;
+        justify-content: space-between;
+        align-items: flex-start;
+        position: fixed;
+        top:20vh;
+        margin-left: -20%;
+    }
+    .sort-btn{
+        cursor: pointer;
+        background: ${({mode}) => mode.background};
+        color: ${({mode}) => mode.color};
+        font-size: 1.5rem;
+        height: 50%;
+        width:70%;
+        border: 2px solid ${({mode}) => mode.color};
+        transition: 0.2s ease-in-out;
+    }
+    .category-name-h1{
+        grid-area: title;
+        font-size: 3rem;
+    }
+    }
+
+    @media  only screen  and (min-width:375px)and (max-width:426px){}
+    @media  only screen  and (min-width:427px)and (max-width:766px){}
+    @media  only screen  and (min-width:767px){}
+    }
+`
