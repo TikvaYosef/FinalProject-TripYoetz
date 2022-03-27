ff1352077a8def348dd3bb234136071036b17fd4
Marcos Bazbih <marcosbaza100@gmail.com>
2022-03-27 19:00:00 +0300

    user profile style

--- client/src/components/pages/UserProfile.jsx
+++ client/src/components/pages/UserProfile.jsx
@@ -1,112 +1,133 @@
-import { useContext, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { MainContext } from "../../contexts/data-context";
 import { StyledUserProfile } from "../styles/pages/StyledUserProfile";
 import EditIcon from "@mui/icons-material/Edit";
 import { userUpdate } from "../../services/user-service";
+import { ThemeContext } from '../../contexts/theme-context'
+import DeleteIcon from '@mui/icons-material/Delete';
+import { removeItemToFavorites } from '../../utils/favoritesList-functions';
+
+
 const UserProfile = () => {
   const { user } = useContext(MainContext);
-  // const [user,usestate]
-  const [prevProfileInfo, SetprevProfileInfo] = useState({...user});
-  const [toggleEdit,setToggleEdit] = useState(false)
-  // console.log(user)
-  const Edit = (event) => {
-    prevProfileInfo[event.target.name] = event.target.value;
+  const { mode } = useContext(ThemeContext);
+  const [profileInfoUpdate, SetProfileInfoUpdate] = useState({ ...user });
+  const [toggleEdit, setToggleEdit] = useState(false);
+  const [favorites, setFavorites] = useState([]);
+
+  useEffect(() => {
+    setFavorites(JSON.parse(localStorage.getItem("favorites")))
+  }, [])
+
+
+  const handleOnChange = (event) => {
+    profileInfoUpdate[event.target.name] = event.target.value;
   };
-  const favorites = JSON.parse(localStorage.getItem("favorites"));
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    SetProfileInfoUpdate({ ...profileInfoUpdate });
+    userUpdate(user._id, user, profileInfoUpdate)
+      .then(() => alert("changed successfully, please log in again to see the changes"));
+  }
+
+  const handleDelete = (favorite) => {
+    console.log(favorite);
+    removeItemToFavorites(favorite);
+    setFavorites(JSON.parse(localStorage.getItem("favorites")));
+  }
+
+
   return (
-    <StyledUserProfile>
-      <div className="mainDiv">
-        <img className="profileImg" src={user.image} alt="img" />
-        <button onClick={()=> setToggleEdit(!toggleEdit)} className="editIcon">
-          <i className="fas fa-edit">
-            {" "}
-            <EditIcon /> edit profile
-          </i>
-        </button>
-      </div>
-      <div className="detailsAndTable">
-        <div className="userDetails">
-          <h1>YOUR INFO</h1>
-          <ul>
-            <h3>
-              <li>{user.name}</li>
-            </h3>
-            <h3>
-              <li>{user.lastName}</li>
-            </h3>
-            <h3>
-              <li>{user.email}</li>
-            </h3>
-            <h3>
-              <li>{user.birthDate.slice(0, 10)}</li>
-            </h3>
-          </ul>
+    <StyledUserProfile mode={mode}>
+      <div className="profile-banner">
+        <div className="profile-img_edit-wrapper">
+          <img className="profile-img" src={user.image} alt={`${user.name} profile`} />
+          <button onClick={() => setToggleEdit(!toggleEdit)} className="edit-user-btn">
+            <EditIcon className="edit-user-icon" /> Edit profile
+          </button>
         </div>
-        <table>
-          <thead>
-            <tr>
-              <th>Items You Liked</th>
-            </tr>
-          </thead>
-          <tbody>
-            {favorites && favorites.length >= 1
-              ? favorites.map((fav, i) => (
-                  <tr key={i}>
-                    <td>
-                      <b>name:</b> {fav.name}
-                    </td>
-                    <td>
-                      <Link to="/itemPage" state={fav._id}>
-                        Link
-                      </Link>
-                    </td>
-                  </tr>
-                ))
-              : null}
-          </tbody>
-        </table>
-        {toggleEdit?
-        <form
-        onSubmit={
-          (event) => {
-            event.preventDefault();
-            SetprevProfileInfo({...prevProfileInfo});
-            userUpdate(user._id, user, prevProfileInfo).then((res) =>
-            console.log(res)
-            );
-          }
-          
-        }
-        >
-          <h1>Edit Your Profile</h1>
-          
-          <input
-            name="name"
-            type="text"
-            onChange={Edit}
-            placeholder="name"
-            minLength={2}
-            maxLength={10}
-          />
-          <input
-            name="lastName"
-            type="text"
-            onChange={Edit}
-            placeholder="lastName"
-            minLength={2}
-            maxLength={10}
-          />
-          <input
-            name="birthDate"
-            type="date"
-            onChange={Edit}
-            placeholder="birthDate"
-          />
-                    <input name="image" type="text" onChange={Edit} placeholder="image" />
-                   <button>SEND</button>
-        </form>:null}
       </div>
+
+      <section className="user-info-wrapper">
+
+        <div className="user-details">
+          <div className="user-details-header">
+            <h1>YOUR INFORMATION</h1>
+          </div>
+          <div className="user-details-body">
+            <h2>Name: {`${user.name} ${user.lastName}`} </h2>
+            <h2>Age: {new Date().getFullYear() - user.birthDate.slice(0, 4)}</h2>
+            <h2>Email: {user.email}</h2>
+            <h2>A member since: {user.createdAt.slice(0, 10)}</h2>
+          </div>
+        </div>
+
+        <div className="user-favorites">
+          <div className="user-favorites-header">
+            <h1>YOUR FAVORITES</h1>
+          </div>
+          {
+            favorites && favorites.length >= 1 ?
+              <table className="favorites-table">
+                <thead>
+                  <tr>
+                    <th>Name</th>
+                    <th>City</th>
+                    <th>Link</th>
+                    <th>Remove</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {
+                    favorites.map((favorite) =>
+                      <tr key={favorite._id}>
+                        <td>{favorite.name}</td>
+                        <td>{favorite.city}</td>
+                        <td><Link to={"/itemPage"} state={favorite._id} className="favorite-link">View more</Link></td>
+                        <td>
+                          <DeleteIcon className="delete-favorite"
+                            onClick={() => handleDelete(favorite)} />
+                        </td>
+                      </tr>
+                    )
+                  }
+                </tbody>
+              </table>
+              :
+              <h1 className="no-favorites-msg">No favorites yet</h1>
+          }
+        </div>
+
+        {
+          toggleEdit
+            ?
+            <div className="user-update-wrapper">
+              <h1>Edit Your Profile</h1>
+              <form className="user-update-form" onSubmit={handleSubmit}>
+                <input name="name" type="text"
+                  onChange={handleOnChange} placeholder="first name"
+                  minLength={2} maxLength={10} />
+
+                <input name="lastName" type="text"
+                  onChange={handleOnChange} placeholder="last name"
+                  minLength={2} maxLength={10} />
+
+                <input name="birthDate" type="date"
+                  onChange={handleOnChange} placeholder="birth date"
+                  min="1902-01-01" max="2004-01-01" />
+
+                <input name="image" type="text"
+                  onChange={handleOnChange} placeholder="profile image" />
+
+                <button>UPDATE</button>
+              </form>
+            </div>
+            :
+            null
+        }
+      </section>
     </StyledUserProfile>
   );
 };
