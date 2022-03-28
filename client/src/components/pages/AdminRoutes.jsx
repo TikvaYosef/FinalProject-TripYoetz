a9d7317acd235f31f709abc971d807865709d9dd
Marcos Bazbih <marcosbaza100@gmail.com>
2022-03-28 14:31:03 +0300

    admin page

--- client/src/components/pages/AdminRoutes.jsx
+++ client/src/components/pages/AdminRoutes.jsx
@@ -1,12 +1,16 @@
+import { useContext } from "react";
 import { Link, Outlet } from "react-router-dom";
+import { ThemeContext } from "../../contexts/theme-context";
 import { StyledAdminNavbar } from '../styles/layout/StyledAdminNavbar'
-
+import SupervisorAccountIcon from '@mui/icons-material/SupervisorAccount';
 const AdminRoutes = () => {
-
+  const { mode } = useContext(ThemeContext);
   return (
     <>
-      <StyledAdminNavbar>
+      <StyledAdminNavbar mode={mode}>
         <ul className="navbar-list" >
+          <li><SupervisorAccountIcon className="admin-icon" /></li>
+          <li><Link className="navbar-link" to="/profile">My Profile</Link></li>
           <li><Link className="navbar-link" to="AdminRestaurants">Restaurants</Link></li>
           <li><Link className="navbar-link" to="AdminHotels">Hotels</Link></li>
           <li><Link className="navbar-link" to="AdminActivities">Activities</Link></li>
--- client/src/components/parts/admin/AdminProfile.jsx
+++ client/src/components/parts/admin/AdminProfile.jsx
@@ -1,30 +1,82 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { MainContext } from "../../../contexts/data-context";
-import { StyledAdmin } from "../../styles/pages/StyledAdmin";
+import { StyledAdminProfile } from "../../styles/pages/StyledAdminProfile";
 import EditIcon from '@mui/icons-material/Edit';
+import { ThemeContext } from "../../../contexts/theme-context";
+import { userUpdate } from "../../../services/user-service";
 
 const AdminProfile = () => {
   const { user } = useContext(MainContext);
+  const { mode } = useContext(ThemeContext);
+  const [profileInfoUpdate, SetProfileInfoUpdate] = useState({ ...user });
+  const [toggleEdit, setToggleEdit] = useState(false);
+
+  const handleToggleEdit = () => {
+    setToggleEdit(!toggleEdit)
+  }
+  const handleOnChange = (event) => {
+    profileInfoUpdate[event.target.name] = event.target.value;
+  };
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    SetProfileInfoUpdate({ ...profileInfoUpdate });
+    userUpdate(user._id, user, profileInfoUpdate)
+      .then(() => alert("changed successfully, please log in again to see the changes"));
+  }
 
   return (
-    <StyledAdmin >
-    <div className="mainDiv">
-    <img className="profileImg" src={user.image} alt="img" />
+    <StyledAdminProfile mode={mode}>
+      <div className="profile-banner">
+        <div className="profile-img_edit-wrapper">
+          <img className="profile-img" src={user.image} alt={`${user.name} profile`} />
+          <button onClick={handleToggleEdit} className="edit-user-btn">
+            <EditIcon className="edit-user-icon" /> Edit profile
+          </button>
+        </div>
+      </div>
 
-  </div>
-  <button className="editIcon">
-          <i className="fas fa-edit"> <EditIcon/> edit profile</i>
-        </button>
-    <div className="userDetails">
-      <h1>YOUR INFO</h1>
-      <ul>
-        <h3><li>{user.name}</li></h3>
-        <h3><li>{user.lastName}</li></h3>
-        <h3><li>{user.email}</li></h3>
-        <h3><li>{user.birthDate.slice(0, 10)}</li></h3>
-      </ul>
-    </div>
-    </StyledAdmin>
+      <section className={`user-info-wrapper ${toggleEdit ? 'responsive' : ''}`}>
+
+        <div className="user-details">
+          <div className="user-details-header">
+            <h1>YOUR INFORMATION</h1>
+          </div>
+          <div className="user-details-body">
+            <h2>Name: {`${user.name} ${user.lastName}`} </h2>
+            <h2>Age: {new Date().getFullYear() - user.birthDate.slice(0, 4)}</h2>
+            <h2>Email: {user.email}</h2>
+            <h2>A member since: {user.createdAt.slice(0, 10)}</h2>
+          </div>
+        </div>
+        {
+          toggleEdit
+            ?
+            <div className="user-update-wrapper">
+              <h1>Edit Your Profile</h1>
+              <form className="user-update-form" onSubmit={handleSubmit}>
+                <input name="name" type="text"
+                  onChange={handleOnChange} placeholder="first name"
+                  minLength={2} maxLength={10} />
+
+                <input name="lastName" type="text"
+                  onChange={handleOnChange} placeholder="last name"
+                  minLength={2} maxLength={10} />
+
+                <input name="birthDate" type="date"
+                  onChange={handleOnChange} placeholder="birth date"
+                  min="1902-01-01" max="2004-01-01" />
+
+                <input name="image" type="text"
+                  onChange={handleOnChange} placeholder="profile image" />
+
+                <button>UPDATE</button>
+              </form>
+            </div>
+            :
+            null
+        }
+      </section>
+    </StyledAdminProfile>
   );
 };
 
